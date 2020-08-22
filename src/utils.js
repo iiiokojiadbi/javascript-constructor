375dec23a1e835521996658a93788f9dad565166
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-22 14:46:20 +0300

    feat: добавить вспомогательную функцию преобразования в css

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,12 @@
 export function row(content, styles = '') {
-  return `<div class='row' style='margin:0; ${styles}'>${content}</div>`
+  return `<div class='row' style='margin:0; ${styles}'>${content}</div>`;
 }
 
 export function col(content) {
-  return `<div class='col-sm' style='padding: 0;'>${content}</div>`
+  return `<div class='col-sm' style='padding: 0;'>${content}</div>`;
+}
+
+export function css(styles = {}) {
+  const toString = (key) => `${key}: ${styles[key]}`;
+  return Object.keys(styles).map(toString).join(';');
 }
